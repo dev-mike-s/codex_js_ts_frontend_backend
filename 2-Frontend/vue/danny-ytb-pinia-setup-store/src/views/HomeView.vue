7ba8c0fa74e0ef82672a2f4f9cbb0eb2f1c8260e
mike.stk <michael.staerke@mail.de>
2026-01-06 23:04:21 +0100

    Add basic exercise 'pinia setup store' for refreshing my know how

---
+++ 2-Frontend/vue/danny-ytb-pinia-setup-store/src/views/HomeView.vue
@@ -0,0 +1,52 @@
+
+<template>
+  <div class="home">
+    <Counter />
+    <div class="buttons">
+      <button @click="storeCounter.decreaseCount">-</button>
+      <button @click="storeCounter.increaseCount">+</button>
+    </div>
+    <hr>
+    <div>
+      The counter is: {{ storeCounter.oddOrEven }}
+    </div>
+    <hr>
+    <h3>Edit counter:</h3>
+    <input 
+      v-model="storeCounter.count"
+      type="number"
+    />
+  </div>
+</template>
+
+<script setup>
+
+  import { ref, computed} from 'vue'
+  import { useCounterStore } from '@/stores/counter'
+  import Counter from '@/components/Counter.vue'
+
+  const storeCounter = useCounterStore()
+
+  const count = ref(0)
+
+  /* for learning purpose (old code) */
+  function increaseCount() {
+    count.value++
+  }
+
+  function decreaseCount() {
+    count.value--
+  }
+
+</script>
+
+<style>
+  .count {
+    font-size: 60px;
+    margin: 20px;
+  }
+  .buttons button {
+    font-size: 40px;
+    margin: 10px;
+  }
+</style>
