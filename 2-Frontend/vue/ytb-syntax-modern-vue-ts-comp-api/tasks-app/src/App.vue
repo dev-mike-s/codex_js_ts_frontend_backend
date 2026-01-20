4a37b33929f8e305dd0bb070ec16155a37652f68
mike.stk <michael.staerke@mail.de>
2026-01-20 22:02:04 +0100

    Init vue 3 proj with ts, vite and composition api

---
+++ 2-Frontend/vue/ytb-syntax-modern-vue-ts-comp-api/tasks-app/src/App.vue
@@ -0,0 +1,43 @@
+<!-- parent component -->
+
+<script lang="ts" setup>
+
+  //imports
+  import { ref } from 'vue';
+  import TaskForm from './components/TaskForm.vue';
+  import type { Task } from './types';
+
+  // variables
+  const message = ref("Tasks App");
+  const tasks = ref<Task[]>([]); // generic type
+
+  // functions
+  function addTask(newTask: string) {
+    console.log(newTask);
+    tasks.value.push({
+      id: crypto.randomUUID(),
+      title: newTask,
+      done: false
+    });
+  }
+
+</script>
+
+<template>
+  <main>
+    <h1> {{ message }}</h1>
+    <TaskForm @add-task="addTask"/>
+    <h3>There are {{ tasks.length  }}  tasks.</h3>
+  </main>
+</template>
+
+<style>
+  main {
+    max-width: 800px;
+    margin: 1rem auto;
+  }
+  .button-container {
+    display: flex;
+    justify-content: end;
+  }
+</style>
---
+++ 2-Frontend/vue/ytb-syntax-modern-vue-ts-comp-api/tasks-app/src/components/TaskForm.vue
@@ -0,0 +1,33 @@
+
+<script lang="ts" setup>
+
+  // imports
+  import { ref } from 'vue';
+
+  // variables
+  const emit = defineEmits<{ addTask: [newTask: string] }>();
+  const newTask = ref("");
+
+  // functions
+  function formSubmitted() {
+    if (newTask.value.trim()) {
+        emit("addTask", newTask.value.trim());
+        newTask.value = ""; 
+    }
+  }
+
+</script>
+
+<template>
+  <main>
+    <form @submit.prevent="formSubmitted">
+      <label>
+        New Task
+        <input v-model="newTask" name="newTask">
+      </label>
+      <div class="button-container">
+        <button>Add</button>
+      </div>
+    </form>
+  </main>
+</template>
